d6ec697cbb070bada177608d0e5a4da0af5776a4
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2022-07-04 20:25:20 +1000

    Update quick-gateway.sh

--- quick-gateway.sh
+++ quick-gateway.sh
@@ -5,6 +5,6 @@ apt-get install iptables iptables-persistent -y
 sleep 2
 iptables -t nat -A POSTROUTING -j MASQUERADE
 sleep 2
-iptables-save &
+iptables-save
+sleep 1
 ping 8.8.8.8 -c 10
-sleep 10; rm quick-gateway.sh &
